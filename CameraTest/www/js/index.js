18f74b17cc38f1a987ee3e587ff092c9d21701b0
raozhiming <raozhiming@elastos.org>
2018-12-14 18:42:31 +0800

    test

--- CameraTest/www/js/index.js
+++ CameraTest/www/js/index.js
@@ -16,10 +16,76 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var divChoosePic = document.getElementById("divChoosePic");
+var imgShow = document.getElementById("imgShow");
+
+//图片选择框显示隐藏
+function showChooseDiv(flag) {
+    if (flag === 1) {//隐藏
+        divChoosePic.style.display = "none";
+        return;
+    }
+    if (flag === 2) {//显示
+        divChoosePic.style.display = "block";
+        return
+    }
+    divChoosePic.style.display = "block";
+}
+
+//拍照
+function callTakePicture() {
+    showChooseDiv(2);
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        //将选择的控件放到要显示的控件上
+        imgShow.src = "data:image/jpeg;base64," + imageData;
+        console.log('-----------------onSuccess-------------\n\n\n------------');
+    };
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    };
+}
+
+//选择本地图片
+function addLocalPicture() {
+    showChooseDiv(2);
+
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    });
+
+    function onSuccess(imageURL) {
+        //将选择的控件放到要显示的控件上
+        imgShow.src = "data:image/jpeg;base64," + imageURL;
+    };
+
+    function onFail(message) {
+        navigator.notification.alert('Failed because:'+message,"操作提示","确定");
+    };
+}
+
+function cancelChoosePic() {
+    showChooseDiv(2);
+    imgShow.src = "img/logo3.png";
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+        // document.getElementById('btnAddImg').addEventListener('click', callTakePicture);
+        document.getElementById('btnChooseFromCamera').addEventListener('click', callTakePicture);
+        document.getElementById('btnChooseFromLocal').addEventListener('click', addLocalPicture);
+        document.getElementById('btnCancelChoose').addEventListener('click', cancelChoosePic);
     },
 
     // deviceready Event Handler
@@ -28,6 +94,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        console.log('-----------------onDeviceReady-------------\n\n\n------------');
     },
 
     // Update DOM on a Received Event
---
+++ NetWorkInformation/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function networkInfo() {
+   var networkState = navigator.connection.type;
+   var states = {};
+
+   states[Connection.UNKNOWN]  = 'Unknown connection';
+   states[Connection.ETHERNET] = 'Ethernet connection';
+   states[Connection.WIFI]     = 'WiFi connection';
+   states[Connection.CELL_2G]  = 'Cell 2G connection';
+   states[Connection.CELL_3G]  = 'Cell 3G connection';
+   states[Connection.CELL_4G]  = 'Cell 4G connection';
+   states[Connection.CELL]     = 'Cell generic connection';
+   states[Connection.NONE]     = 'No network connection';
+
+   alert('Connection type: ' + states[networkState]);
+}
+
+function onOffline() {
+   alert('You are now offline!');
+}
+
+function onOnline() {
+   alert('You are now online!');
+}
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.getElementById("networkInfo").addEventListener("click", networkInfo);
+        document.addEventListener("offline", onOffline, false);
+        document.addEventListener("online", onOnline, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        console.log('-----------------onDeviceReady-------------\n\n\n------------');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    }
+};
+
+app.initialize();
